ce6dad5ec676e86cf994fdbe3b30a0ff3b15b6d3
Himanshu Singhal <himanshu.singhal@rocketmail.com>
2017-03-17 17:50:51 +0530

    almost version

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -1,6 +1,20 @@
 import Vapor
 
 let drop = Droplet()
+var fname:String = String()
+var lname:String = String()
+var eid:String = String()
+var pno:String = String()
+var add:String = String()
+var jtitle:String = String()
+var comp:String = String()
+var esd:String = String()
+var eed:String = String()
+var cname:String = String()
+var iname:String = String()
+var eded:String = String()
+var edsd:String = String()
+var skil:String = String()
 drop.middleware.insert(CORSMiddleware(), at: 0)
 drop.get { req in
     return try drop.view.make("welcome", [
@@ -8,22 +22,45 @@ drop.get { req in
     ])
 }
 
+drop.get("resume") { req in
+    return try drop.view.make("resume", Node(node: ["firstname": fname
+                                                ,"lastname": lname
+                                                ,"emailid": eid
+                                                ,"phoneno": pno
+                                                ,"address": add
+                                                ,"jobtitle": jtitle
+                                                ,"company": comp
+                                                ,"sdate": esd
+                                                ,"edate": eed
+                                                ,"course": cname
+                                                ,"institute": iname
+                                                ,"startdate": edsd
+                                                ,"enddate": eded
+                                                ,"skill": skil
+    ]))
+}
+
 drop.post("template2") { request in
   guard let firstname = request.data["firstname"]?.string else {
     throw Abort.badRequest
   }
+  fname = firstname
   guard let lastname = request.data["lastname"]?.string else {
     throw Abort.badRequest
   }
+  lname = lastname
   guard let emailid = request.data["emailid"]?.string else {
     throw Abort.badRequest
   }
+  eid = emailid
   guard let phoneno = request.data["phoneno"]?.string else {
     throw Abort.badRequest
   }
+  pno = phoneno
   guard let address = request.data["address"]?.string else {
     throw Abort.badRequest
   }
+  add = address
   return try drop.view.make("template1", Node(node: ["firstname": firstname
                                               ,"lastname": lastname
                                               ,"emailid": emailid
@@ -35,15 +72,18 @@ drop.post("entry") { request in
   guard let jobtitle = request.data["jobtitle"]?.string else {
     throw Abort.badRequest
   }
+  jtitle = jobtitle
   guard let company = request.data["company"]?.string else {
     throw Abort.badRequest
   }
   guard let startdate = request.data["startdate"]?.string else {
     throw Abort.badRequest
   }
+  esd = startdate
   guard let enddate = request.data["enddate"]?.string else {
     throw Abort.badRequest
   }
+  eed = enddate
   return try drop.view.make("entry", Node(node: ["jobtitle": jobtitle
                                               ,"company": company
                                               ,"startdate": startdate
@@ -54,15 +94,19 @@ drop.post("education") { request in
   guard let coursename = request.data["coursename"]?.string else {
     throw Abort.badRequest
   }
+  cname = coursename
   guard let institute = request.data["institute"]?.string else {
     throw Abort.badRequest
   }
+  iname = institute
   guard let startdate = request.data["startdate"]?.string else {
     throw Abort.badRequest
   }
+  edsd = startdate
   guard let enddate = request.data["enddate"]?.string else {
     throw Abort.badRequest
   }
+  eded = enddate
   return try drop.view.make("education", Node(node: ["coursename": coursename
                                               ,"institute": institute
                                               ,"startdate": startdate
@@ -73,6 +117,7 @@ drop.post("skills") { request in
   guard let skill = request.data["skill"]?.string else {
     throw Abort.badRequest
   }
+  skil = skill
   return skill
 }
 
